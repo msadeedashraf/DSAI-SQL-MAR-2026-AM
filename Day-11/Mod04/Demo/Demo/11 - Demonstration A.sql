141e41a866547752966ca5e604dd49c3ebef2502
Sadeed <msadeedashraf@gmail.com>
2026-03-16 11:25:20 -0400

    .

--- Day-11/Mod04/Demo/Demo/11 - Demonstration A.sql
+++ Day-11/Mod04/Demo/Demo/11 - Demonstration A.sql
@@ -4,6 +4,95 @@
 USE TSQL;
 GO
 
+
+--SQL89
+select * from hr.employees
+
+select * from tablename
+where category_name = 'Dairy'
+
+
+select * from Production.Products as P -- total 77rows--6 clmn
+where P.categoryid = 1
+
+--Dairy Products--10 rows
+--Beverages--12 
+
+select * from Production.Categories --total 8 rows--3 clmn
+
+
+--SQL 89
+select 
+P.*,
+C.*
+from Production.Products as P, Production.Categories as C
+--SQL 89
+select 
+P.productid,
+P.productname,
+P.unitprice,
+C.categoryname,
+C.description
+from Production.Products as P, Production.Categories as C
+where P.categoryid = C.categoryid
+and C.categoryname = 'Dairy Products'
+
+--SQL92
+select 
+P.productid,
+P.productname,
+P.unitprice,
+C.categoryname,
+C.description
+from 
+Production.Products as P  join  Production.Categories as C
+on P.categoryid = C.categoryid
+where 
+C.categoryname = 'Dairy Products'
+
+
+Beverages
+Condiments
+Confections
+Dairy Products
+Grains/Cereals
+Meat/Poultry
+Produce
+Seafood
+
+select 
+P.productid,
+P.productname,
+P.unitprice,
+S.contactname,
+S.city,
+S.country
+from Production.Products as P join Production.Suppliers as S
+on P.supplierid = S.supplierid
+where 
+S.contactname = 'Holm, Michael'
+
+
+
+select 
+P.productid,
+P.productname,
+P.unitprice,
+C.categoryname,
+C.description
+from 
+Production.Products as P  join  Production.Categories as C
+on P.categoryid = C.categoryid
+where 
+C.categoryname = 'Dairy Products'
+
+
+--SQL92
+
+
+---there is only one join
+
+
 -- Step 2: Join 2 tables
 -- Select and execute the following query
 -- to illustrate ANSI SQL-89 syntax
